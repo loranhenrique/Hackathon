6fb4e3425bc1eedf4cdcd6e3e48cd2c119c804a6
João <juliobento237@gmail.com>
2019-10-16 15:28:21 -0300

    Atualização final tela de login

--- frontend/src/pages/LoginEscola/index.js
+++ frontend/src/pages/LoginEscola/index.js
@@ -1,46 +1,60 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
-export default function LoginEscola({ history }){
+export default function LoginAluno({ history }) {
     const [matricula, setMatricula] = useState('');
     const [senha, setSenha] = useState('');
-  
-    async function handleSubmit(event){
-      event.preventDefault();
-  
-      const response = await api.post('/escolas/authenticate', {matricula, senha});
-  
-      const { _id } = response.data;
 
-      localStorage.setItem('escola', _id);
+    async function handleSubmit(event) {
+        event.preventDefault();
 
-      history.push('/Escola');
+        const response = await api.post('/escolas/authenticate', { matricula, senha });
+
+        const { _id } = response.data;
+
+        localStorage.setItem('escolas', _id);
+
+        history.push('/Escola');
     }
 
-    return(
+    return (
         <>
-        <div className="contenteAluno">
-            <p>FamInSchool</p>
+            <div className="backLogin">
+                <div className="contentAluno" align="center">
+                    <div id="logoLogin"></div>
 
-            <form onSubmit={handleSubmit}>
-                <input type="number" 
-                id="matricula" 
-                placeholder="Matricula" 
-                value={matricula} 
-                onChange={ event => setMatricula(event.target.value)}
-                />
-                <input type="password"
-                id="senha" 
-                placeholder="Password" 
-                value={senha} 
-                onChange={ event => setSenha(event.target.value)}
-                />
-                <button type="submit" className="btn">CONFIRMAR</button>
-                <button type="submit" className="btnSenha">Esqueceu a senha?</button>
-            </form>
-        </div>
+                    <p id="escolha">Escola como deseja entrar:</p>
+                    <div className="acessos">
+                        <li>
+                            <Link to="/LoginEscola"><button className="btnLinkAluno">Escola</button></Link>
+                            <Link to="/LoginProfessor"><button className="btnLink">Professor</button></Link>
+                            <Link to="/LoginResponsavel"><button className="btnLink">Responsável</button></Link>
+                            <Link to="/LoginAluno"><button className="btnLink">Aluno</button></Link>
+                        </li>
+                    </div>
+
+                    <form onSubmit={handleSubmit}>
+                        <input type="number"
+                            id="matricula"
+                            placeholder="Matricula"
+                            value={matricula}
+                            onChange={event => setMatricula(event.target.value)}
+                        />
+                        <input type="password"
+                            id="senha"
+                            placeholder="Password"
+                            value={senha}
+                            onChange={event => setSenha(event.target.value)}
+                        />
+                        <button type="submit" className="btnProfessor">CONFIRMAR</button>
+                        <button type="submit" className="btnSenha">Esqueceu a senha?</button>
+                        <button type="submit" className="btnCriarConta">CRIAR UMA CONTA</button>
+                    </form>
+                </div>
+            </div>
         </>
     );
 }
