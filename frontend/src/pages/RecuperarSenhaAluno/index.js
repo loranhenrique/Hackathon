affeb02795e1915486c78107cdf5d526c231c76b
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2019-10-24 11:11:48 -0300

    Atualização na resposividade de todas as telas

---
+++ frontend/src/pages/RecuperarSenhaAluno/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+export default function RecuperarSenhaAluno({ history }) {
+    const [email, setEmail] = useState('');
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        await api.post('/Aluno/authenticate', { email });
+
+        history.push('/ConfirmarSenhaAluno'); 
+    }
+
+    return (
+        <>
+            <div className="backLogin">
+                <div className="contentRecuperarSenha" align="center">
+                    <div id="logoLogin"></div>
+
+                    <p id="label">Recuperação de senha</p>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text"
+                            id="email"
+                            placeholder="Digite seu E-mail"
+                            value={email}
+                            onChange={event => setEmail(event.target.value)}
+                        />
+                        <Link to="/ConfirmarSenhaAluno"><button type="submit" className="btnConfirmEmail2">CONFIRMAR</button></Link>
+                        <Link to="/LoginAluno"><button type="submit" className="btnLinkVoltar2">VOLTAR</button></Link>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -11,8 +11,14 @@ import Professor from './pages/Professor';
 import Escola from './pages/Escola';
 import CriarContaResponsavel from './pages/CriarContaResponsavel';
 import CriarContaEscola from './pages/CriarContaEscola';
-import RecuperarSenha from './pages/RecuperarSenha';
-import ConfirmarSenha from './pages/ConfirmarSenha';
+import RecuperarSenhaAluno from './pages/RecuperarSenhaAluno';
+import RecuperarSenhaEscola from './pages/RecuperarSenhaEscola';
+import RecuperarSenhaProfessor from './pages/RecuperarSenhaProfessor';
+import RecuperarSenhaResponsavel from './pages/RecuperarSenhaResponsavel';
+import ConfirmarSenhaAluno from './pages/ConfirmarSenhaAluno';
+import ConfirmarSenhaEscola from './pages/ConfirmarSenhaEscola';
+import ConfirmarSenhaResponsavel from './pages/ConfirmarSenhaResponsavel';
+import ConfirmarSenhaProfessor from './pages/ConfirmarSenhaProfessor';
 
 
 export default function Routes() {
@@ -30,8 +36,14 @@ export default function Routes() {
                 <Route path="/Escola" component={Escola} />
                 <Route path="/CriarContaResponsavel" component={CriarContaResponsavel} />
                 <Route path="/CriarContaEscola" component={CriarContaEscola} />
-                <Route path="/RecuperarSenha" component={RecuperarSenha} />
-                <Route path="/ConfirmarSenha" component={ConfirmarSenha} />
+                <Route path="/RecuperarSenhaAluno" component={RecuperarSenhaAluno} />
+                <Route path="/RecuperarSenhaEscola" component={RecuperarSenhaEscola} />
+                <Route path="/RecuperarSenhaProfessor" component={RecuperarSenhaProfessor }/>
+                <Route path="/RecuperarSenhaResponsavel" component={RecuperarSenhaResponsavel} />
+                <Route path="/ConfirmarSenhaAluno" component={ConfirmarSenhaAluno} />
+                <Route path="/ConfirmarSenhaEscola" component={ConfirmarSenhaEscola} />
+                <Route path="/ConfirmarSenhaResponsavel" component={ConfirmarSenhaResponsavel} />
+                <Route path="/ConfirmarSenhaProfessor" component={ConfirmarSenhaProfessor} />
             </Switch>
         </BrowserRouter>
     );
