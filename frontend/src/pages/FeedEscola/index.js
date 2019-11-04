728845ed6bcf64e374710c8bd358200db78ffee6
Loran Henrique <loranteixeira@icloud.com>
2019-11-04 15:55:14 -0300

    Escola já consegue cadastrar novo aviso

--- frontend/src/pages/FeedEscola/index.js
+++ frontend/src/pages/FeedEscola/index.js
@@ -1,35 +1,39 @@
-import React, { useEffect, useState, useMemo } from 'react';
-import api from '../../services/api';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './styles.css';
-
-//importando imagens que será usada no menu como logo
-import home from '../../assets/home.png';
-import calendario from '../../assets/calendario.png';
-import notas from '../../assets/notas.png';
 import aulas from '../../assets/aulas_quadro.png';
+import calendario from '../../assets/calendario.png';
+import camera from '../../assets/camera.svg';
 import escola_icon from '../../assets/escola.png';
 import escola2 from '../../assets/escola2.png';
-import camera from '../../assets/camera.svg';
-
-
-
-
+//importando imagens que será usada no menu como logo
+import home from '../../assets/home.png';
+import notas from '../../assets/notas.png';
+import api from '../../services/api';
+import './styles.css';
 
 export default function FeedEscola() {
-    const [foto, setFoto] = useState(null);
+    const [imgPost, setImgPost] = useState(null);
     const [avisos, setAvisos] = useState([]);
     const [escola, setEscola] = useState('');
-    const [post, setPost] = useState('');
+    const [mensagem, setMensagem] = useState('');
 
-    function handleSubmit(){
 
+    async function handleSubmit(){
+        const data = new FormData();
+        const escola_id = localStorage.getItem('escolas');
+
+        data.append('image', imgPost);
+        data.append('mensagem', mensagem);
+
+        await api.post('/avisos/register', data, {
+            headers: { escola_id}
+        })
     }
 
 
     const preview = useMemo(() => {
-        return foto ? URL.createObjectURL(foto) : null;
-      }, [foto])
+        return imgPost ? URL.createObjectURL(imgPost) : null;
+      }, [imgPost])
 
     useEffect(() => {
         async function loadAvisos() {
@@ -82,25 +86,30 @@ export default function FeedEscola() {
                 <div className="campo-texto">
                     <div className="campo-texto2">
                     <img className="icon" src={escola2} alt= {escola.nome} />
+
+
+                    {/*Começa o formulario para adicionar novo aviso*/}
                     <form onSubmit={handleSubmit}>
                     <input type="Text"
                             id="aviso_com_foto"
                             placeholder="Add a post"
-                            value={post}
-                            onChange={event => setPost(event.target.value)}
-                        />
-                        <button className="novoAviso" >Publicar</button>
-      <label 
-        id="foto" 
-        style={{ backgroundImage: `url(${preview})` }}
-        className={foto ? 'has-foto' : ''}
-      >
-        <input type="file" onChange={event => setFoto(event.target.files[0])} />
-        <img src={camera} alt="Select img" />
-      </label>
-      
-      </form> 
-      </div>
+                            value={mensagem}
+                            onChange={event => setMensagem(event.target.value)}
+                    />
+                    <button className="novoAviso" >Publicar</button>
+                    <label 
+                        id="foto" 
+                        style={{ backgroundImage: `url(${preview})` }}
+                        className={imgPost ? 'has-foto' : ''}
+                    >
+                        <input type="file" onChange={event => setImgPost(event.target.files[0])} />
+                        <img src={camera} alt="Select img" />
+                    </label>
+                    </form> 
+
+
+
+                    </div>
                 </div>
                 <div className="posts">
                     <ul className="feed_post">
